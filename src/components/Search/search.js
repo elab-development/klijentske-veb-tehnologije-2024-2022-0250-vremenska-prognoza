81406d49b07bd54747945cb4f9ae4613c1278426
djolee65 <djordjeisakovic12@gmail.com>
2025-10-22 18:55:32 +0200

    Cuvanje poslednje pretrage

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { AsyncPaginate } from "react-select-async-paginate"
 import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
     const [search, setSearch] = useState(null);
+
+    useEffect(() => {
+        const savedSearch = localStorage.getItem("lastSearch");
+        if (savedSearch) {
+            try {
+                const parsed = JSON.parse(savedSearch);
+                setSearch(parsed);
+                onSearchChange(parsed); // automatski poziva fetch podataka
+            } catch (err) {
+                console.error("Greška pri učitavanju poslednje pretrage:", err);
+            }
+        }
+    }, [onSearchChange]);
 
     const loadOptions = (inputValue) => {
         return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
@@ -13,7 +26,7 @@ const Search = ({onSearchChange}) => {
                     options: response.data.map((city) => {
                         return {
                             value: `${city.latitude} ${city.longitude}`,
-                            label: `${city.name}, ${city.countryCode}` ,
+                            label: `${city.name}, ${city.countryCode}`,
                         }
                     })
                 }
@@ -24,6 +37,7 @@ const Search = ({onSearchChange}) => {
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
+        localStorage.setItem("lastSearch", JSON.stringify(searchData));
     }
     return (
         <AsyncPaginate
