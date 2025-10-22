9352006900658a5ab4f767f428454b804e1f647b
djolee65 <djordjeisakovic12@gmail.com>
2025-10-22 03:49:47 +0200

    Funkcionalnost search bar-a

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'd0fa20b4a3msh98f3a7d484f1131p1fe0f4jsn7333b01a8920',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
