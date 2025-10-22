16658a65ac3a9bb6f9873edb6a71e374de2cf43a
djolee65 <djordjeisakovic12@gmail.com>
2025-10-22 04:40:21 +0200

    Css trenutno vreme

--- src/components/trenutno-vreme/trenutno-vreme.js
+++ src/components/trenutno-vreme/trenutno-vreme.js
@@ -4,10 +4,36 @@ const TrenutnoVreme= () => {
     return (
         <div className="vreme">
             <div className="top">
-                <p className="grad">Belgrade</p>
-                <p className="vreme-opis">Sunny</p>
+                <div>
+                    <p className="grad">Belgrade</p>
+                    <p className="vreme-opis">Sunny</p>
+                </div>
+                <img alt="vreme" className="vreme-icon" src="icons/01d.png"/>
             </div>
-            <img alt="vreme" className="vreme-icon" src="icons/01d.png"/>
+            <div className="donji">
+                <p className="temperatura">18 °C</p>
+                <div className="detalji">
+                   <div className="parametri-red">
+                        <span className="parametri">Detalji</span>
+                    </div>
+                    <div className="parametri-red">
+                        <span className="parametri">Osecaj</span>
+                        <span className="parametri-vrednost">22 °C</span>
+                    </div>
+                    <div className="parametri-red">
+                        <span className="parametri">Vetar</span>
+                        <span className="parametri-vrednost">2 m/s</span>
+                    </div>
+                    <div className="parametri-red">
+                        <span className="parametri">Vlaznost</span>
+                        <span className="parametri-vrednost">15%</span>
+                    </div>
+                    <div className="parametri-red">
+                        <span className="parametri">Pritisak</span>
+                        <span className="parametri-vrednost">15 hPa</span>
+                    </div>
+                </div> 
+            </div>
         </div>
     );
 }
